1f39f82612f2af1b81200d41984f4b3c62207fc9
filip <25711588+Filip10@users.noreply.github.com>
2024-01-02 17:34:20 +0100

    init

--- cmake-build-debug/CMakeFiles/semestralka.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/semestralka.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/tmp/tmp.Cc70vDlGMv/main.c" "/tmp/tmp.Cc70vDlGMv/cmake-build-debug/CMakeFiles/semestralka.dir/main.c.o"
+  "/tmp/tmp.c4JwXTXy8h/main.c" "/tmp/tmp.c4JwXTXy8h/cmake-build-debug/CMakeFiles/semestralka.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
