3dd5538b0a6b5c5dcb85875686bd75a434f128ae
filip <25711588+Filip10@users.noreply.github.com>
2024-01-03 17:43:33 +0100

    client.c upgrade

--- client.c
+++ client.c
@@ -1,32 +1,59 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-#define SERV_TCP_PORT 5035
+#define SERV_TCP_PORT 15038
 #define MAX 60
-int main(int arg,char*argv[])
-{
-    int sockfd,n;
+
+int main() {
+    int sockfd, n;
     struct sockaddr_in serv_addr;
-    struct hostentserver;
-    char send[MAX],recvline[MAX],s[MAX],name[MAX];
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    serv_addr.sin_port=htons(SERV_TCP_PORT);
-    connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
-    printf("\nEnter the source file name : \n");
-    scanf("%s",send);
-    write(sockfd,send,MAX);
-    while((n=read(sockfd,recvline,MAX))!=0)
-    {
-        printf("%s",recvline);
+    char send[MAX], recvline[MAX];
+
+    // Replace "127.0.0.1" with your server's IP address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(SERV_TCP_PORT);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
+        perror("Connection failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter the source file name: ");
+    if (scanf(" %s", send) != 1) {
+        fprintf(stderr, "Error reading file name\n");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(sockfd, send, MAX) == -1) {
+        perror("Write to server failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((n = read(sockfd, recvline, MAX)) > 0) {
+        printf("%.*s", n, recvline);
+    }
+
+    if (n == -1) {
+        perror("Read from server failed");
+    }
+
     close(sockfd);
     return 0;
 }
