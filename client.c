6dece3fcbf3a81f20a0e8e337e69c3cc81125ea1
Lukas <80598998+bluMister@users.noreply.github.com>
2024-01-03 16:35:36 +0100

    hotfix

--- client.c
+++ client.c
@@ -6,6 +6,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include <arpa/inet.h>
 
 #define SERV_TCP_PORT 5035
 #define MAX 60
@@ -17,6 +18,7 @@ int main(int arg,char*argv[])
     char send[MAX],recvline[MAX],s[MAX],name[MAX];
     sockfd=socket(AF_INET,SOCK_STREAM,0);
     serv_addr.sin_family=AF_INET;
+
     serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
     serv_addr.sin_port=htons(SERV_TCP_PORT);
     connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
