0a5e0ed1c24bd61f42e8927001d8fdef71aa82da
Lukas <80598998+bluMister@users.noreply.github.com>
2024-01-04 15:21:38 +0100

    https

--- client.c
+++ client.c
@@ -4,16 +4,23 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define BUFFER_SIZE 1024
 
 // Function to extract hostname and path from the URL
-int parse_url(const char *url, char *hostname, char *path) {
-    if (sscanf(url, "http://%[^/]/%s", hostname, path) != 2) {
-        fprintf(stderr, "Invalid URL format\n");
-        return 1;
+int parse_url(const char *url, char *hostname, char *path, int *is_https) {
+    if (sscanf(url, "http://%[^/]/%s", hostname, path) == 2) {
+        *is_https = 0;
+        return 0;
+    } else if (sscanf(url, "https://%[^/]/%s", hostname, path) == 2) {
+        *is_https = 1;
+        return 0;
     }
-    return 0;
+
+    fprintf(stderr, "Invalid URL format\n");
+    return 1;
 }
 
 // Function to extract filename from the URL
@@ -22,13 +29,24 @@ const char *extract_filename(const char *url) {
     return (last_slash != NULL) ? (last_slash + 1) : url;
 }
 
-// Function to download a file from an HTTP server
+// Function to set up SSL context
+SSL_CTX *create_ssl_context() {
+    SSL_CTX *ctx = SSL_CTX_new(SSLv23_client_method());
+    if (!ctx) {
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+    return ctx;
+}
+
+// Function to download a file from an HTTP or HTTPS server
 int download_file(const char *url) {
     char hostname[256];
     char path[256];
+    int is_https;
 
     // Parse URL to extract hostname and path
-    if (parse_url(url, hostname, path) != 0) {
+    if (parse_url(url, hostname, path, &is_https) != 0) {
         return 1;
     }
 
@@ -49,7 +67,7 @@ int download_file(const char *url) {
     // Set up server address structure
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(80);
+    server_addr.sin_port = htons(is_https ? 443 : 80);
     memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
 
     // Connect to the server
@@ -59,14 +77,49 @@ int download_file(const char *url) {
         return 1;
     }
 
+    // If using HTTPS, set up SSL
+    SSL_CTX *ssl_ctx = NULL;
+    SSL *ssl = NULL;
+    if (is_https) {
+        SSL_library_init();
+        ssl_ctx = create_ssl_context();
+        if (!ssl_ctx) {
+            close(sockfd);
+            return 1;
+        }
+
+        ssl = SSL_new(ssl_ctx);
+        SSL_set_fd(ssl, sockfd);
+
+        if (SSL_connect(ssl) != 1) {
+            ERR_print_errors_fp(stderr);
+            SSL_free(ssl);
+            SSL_CTX_free(ssl_ctx);
+            close(sockfd);
+            return 1;
+        }
+    }
+
     // Send HTTP GET request
-    dprintf(sockfd, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, hostname);
+    if (is_https) {
+        SSL_write(ssl, "GET /", 5);
+        SSL_write(ssl, path, strlen(path));
+        SSL_write(ssl, " HTTP/1.1\r\nHost: ", 17);
+        SSL_write(ssl, hostname, strlen(hostname));
+        SSL_write(ssl, "\r\n\r\n", 4);
+    } else {
+        dprintf(sockfd, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, hostname);
+    }
 
     // Open the output file with the extracted filename
     const char *filename = extract_filename(url);
     FILE *file = fopen(filename, "wb");
     if (file == NULL) {
         perror("Error opening output file");
+        if (is_https) {
+            SSL_free(ssl);
+            SSL_CTX_free(ssl_ctx);
+        }
         close(sockfd);
         return 1;
     }
@@ -74,12 +127,27 @@ int download_file(const char *url) {
     // Receive and save the file data
     char buffer[BUFFER_SIZE];
     ssize_t bytesRead;
-    while ((bytesRead = read(sockfd, buffer, sizeof(buffer))) > 0) {
+    if (is_https) {
+        bytesRead = SSL_read(ssl, buffer, sizeof(buffer));
+    } else {
+        bytesRead = read(sockfd, buffer, sizeof(buffer));
+    }
+
+    while (bytesRead > 0) {
         fwrite(buffer, 1, bytesRead, file);
+        if (is_https) {
+            bytesRead = SSL_read(ssl, buffer, sizeof(buffer));
+        } else {
+            bytesRead = read(sockfd, buffer, sizeof(buffer));
+        }
     }
 
     // Clean up
     fclose(file);
+    if (is_https) {
+        SSL_free(ssl);
+        SSL_CTX_free(ssl_ctx);
+    }
     close(sockfd);
 
     printf("Download complete. File saved as %s\n", filename);
--- cmake-build-debug/CMakeFiles/client.dir/build.make
+++ cmake-build-debug/CMakeFiles/client.dir/build.make
@@ -98,6 +98,8 @@ client_EXTERNAL_OBJECTS =
 
 client: CMakeFiles/client.dir/client.c.o
 client: CMakeFiles/client.dir/build.make
+client: /usr/lib/x86_64-linux-gnu/libssl.so
+client: /usr/lib/x86_64-linux-gnu/libcrypto.so
 client: CMakeFiles/client.dir/link.txt
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green --bold --progress-dir=/tmp/tmp.oqyhl6GXco/cmake-build-debug/CMakeFiles --progress-num=$(CMAKE_PROGRESS_2) "Linking C executable client"
 	$(CMAKE_COMMAND) -E cmake_link_script CMakeFiles/client.dir/link.txt --verbose=$(VERBOSE)
